1e711f376a524f74878ccd05aa4a778d43c27b05
Justin Hu <50312952+TheBBman@users.noreply.github.com>
2023-06-02 22:35:03 -0700

    Fragmentation cases
    Probably works...?

--- project/main.cpp
+++ project/main.cpp
@@ -12,14 +12,15 @@
 #include <unordered_map>
 
 #define DEFAULT_PORT 5152
+#define MAX_CLIENTS 10
 
 #pragma pack(1)
-struct IP_packet{
+struct IP_header{
     uint8_t version :4;
     uint8_t header_length :4;
     uint8_t stuff;
     uint16_t total_length;
-    uint32_t more_stuff2;
+    uint32_t more_stuff;
     uint8_t TTL;
     uint8_t protocol;
     uint16_t ip_checksum;
@@ -44,7 +45,7 @@ int main() {
     
     // Store all local host IP addresses in order
     int num_clients = 0;
-    std::string client_ips[30];    // Too lazy, just set some max client count
+    std::string client_ips[MAX_CLIENTS];    // Too lazy, just set some max client count
 
     // Step 1: Get all local host IP addresses
     while (1) {
@@ -56,7 +57,7 @@ int main() {
         num_clients++;
 
         // Debug use
-        if (num_clients > 30) {
+        if (num_clients > MAX_CLIENTS) {
             perror("Max clients reached!");
             exit(1);
         }
@@ -99,8 +100,6 @@ int main() {
     // Socket to get connections and list of client sockets
     int listening_socket, client_sockets[num_clients];
     fd_set readfds;
-    // Single ip packet maximum size
-    char buffer[65535];
 
     listening_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (listening_socket == 0) {
@@ -148,4 +147,155 @@ int main() {
     //--------------------------------------------------------------------------------------//
     // Poll connections with select() 
 
+    // Needed for select(), highest socket # in the list
+    int max_sd = 0;
+
+    // Single ip packet maximum size, to be reused for processing
+    // Will only ever hold at most one ip packet
+    char buffer[num_clients][65535];
+
+    // Used to keep track of partial ip packets ^_^
+    int fragment[num_clients] = {0};
+    int bytes_fragmented[num_clients];
+
+    while (1) {
+        // Zero out fd set and then add all connections into it
+        FD_ZERO(&readfds);
+        for(int i = 0; i < num_clients; i++) {
+            int sd = client_sockets[i];
+            FD_SET(sd, &readfds);
+            if (sd > max_sd) {
+                max_sd = sd;
+            }
+        }
+
+        // Only select sockets with something to read
+        int activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
+        if ((activity < 0) && (errno != EINTR)) {
+            perror("select");
+        }
+
+        // Loop through all client sockets
+        for (int i = 0; i < num_clients; i++) {
+            int sd = client_sockets[i];
+
+            // If socket was NOT selected, skip
+            if (!FD_ISSET(sd, &readfds)) {
+                continue;
+            }
+
+            // end of select()
+            //--------------------------------------------------------------------------------------//
+            // Extract single, valid ip packet
+
+            // How many bytes into available stream has been read
+            int sd_read_head = 0;
+
+            //-----------------------------------------------------------------//
+            // Fragmentation cases, will only happen once per select() so just hard code
+
+            // Fragmented header 
+            if (fragment[i] == 1) {
+                int bytes = bytes_to_read[i];
+
+                // Read in next ip packet header
+                int bytes_read = read(sd, buffer[i] + 20 - bytes, bytes);
+                sd_read_head += bytes_read;
+
+                // This means ip header fragmented, AGAIN
+                else if (bytes_read < 20) {
+                    fragment[i] = 1;
+                    bytes_fragmented[i] = 20 - bytes - bytes_read;
+                    continue;
+                }
+                // Normal logic
+
+                // Find total length of ip packet
+                const struct IP_header* ip = (const struct IP_header*)buffer[i];
+                uint16_t payload_length = ntohs(ip->total_length) - 20;
+                
+                bytes_read = read(sd, buffer[i] + sd_read_head, payload_length);
+                
+                // This means ip payload fragmented (bruh)
+                if (bytes_read < payload_length) {
+                    fragment[i] = 2;
+                    bytes_fragmented[i] = payload_length - bytes_read;
+                    continue;
+                }
+
+                // Now buffer should contain a full ip packet
+
+                // ---> Randy doing his magic <--- //
+
+                // Reset buffer back to known state
+                memset(buffer[i], 0, 65535);
+            }
+            // Fragmented payload 
+            else if (fragment[i] == 2) {
+                const struct IP_header* ip = (const struct IP_header*)buffer[i];
+                uint16_t payload_length = ntohs(ip->total_length) - 20;
+
+                int head_position = payload_length - bytes_to_read[i]
+                
+                int bytes_read = read(sd, buffer[i] + head_position, bytes_to_read[i]);
+                
+                // This means ip payload fragmented, again
+                if (bytes_read < payload_length) {
+                    fragment[i] = 2;
+                    bytes_fragmented[i] = payload_length - bytes_read;
+                    continue;
+                }
+
+                sd_read_head += bytes_read;
+
+                // Now buffer should contain a full ip packet
+
+                // ---> Randy doing his magic <--- //
+
+                // Reset buffer back to known state
+                memset(buffer[i], 0, 65535);
+            }
+
+            //-----------------------------------------------------------------//
+
+            // Could be multiple packets in stream
+            while(1) {
+                // Read in next ip packet header
+                int bytes_read = read(sd, buffer[i] + sd_read_head, 20);
+                sd_read_head += bytes_read;
+
+                // This probably means test is over, wait for SIGKILL
+                if (bytes_read == 0) {
+                    break;;
+                }
+                // This means ip header fragmented 
+                else if (bytes_read < 20) {
+                    fragment[i] = 1;
+                    bytes_fragmented[i] = 20 - bytes_read;
+                    break;
+                }
+                // Normal logic
+
+                // Find total length of ip packet
+                const struct IP_header* ip = (const struct IP_header*)buffer[i];
+                uint16_t payload_length = ntohs(ip->total_length) - 20;
+                
+                bytes_read = read(sd, buffer[i] + sd_read_head, payload_length);
+                
+                // This means ip payload fragmented
+                if (bytes_read < payload_length) {
+                    fragment[i] = 2;
+                    bytes_fragmented[i] = payload_length - bytes_read;
+                    break;
+                }
+
+                // Now buffer should contain a full ip packet
+
+                // ---> Randy doing his magic <--- //
+
+                // Reset buffer back to known state
+                memset(buffer[i], 0, 65535);
+            }
+        }
+    }
 }
