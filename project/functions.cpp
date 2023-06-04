bdf7884fb1c81e2dd070d5016033423f577ff678
rgu855 <rgu865@gmail.com>
2023-06-03 21:58:02 -0700

    fixed checksums fr

--- project/functions.cpp
+++ project/functions.cpp
@@ -14,16 +14,16 @@
 using namespace std;
 #pragma pack(1)
 struct IP_header{
-    uint8_t version:4;
     uint8_t header_length :4;
-    uint8_t types_of_service;
-    uint16_t total_length;
-    uint32_t stuff;
+    uint8_t version:4;
+    uint8_t types_of_service; //69
+    uint16_t total_length; //7237
+    uint32_t stuff; //7237 7301
     uint8_t TTL;
-    uint8_t protocol;
-    uint16_t ip_checksum;
-    uint32_t source_ip;
-    uint32_t dest_ip;
+    uint8_t protocol; //11717
+    uint16_t ip_checksum; //11717
+    uint32_t source_ip; //54917, 80518
+    uint32_t dest_ip; //123718, 174919
 };
 #pragma pack()
 
@@ -33,8 +33,8 @@ struct TCP_header{
     uint16_t source_port;
     uint16_t dest_port;
     uint64_t stuffers;
-    uint8_t header_length :4;
     uint8_t stuffies:4;
+    uint8_t header_length :4;
     uint8_t random;
     uint16_t window;
     uint16_t TCP_checksum;
@@ -72,12 +72,18 @@ uint16_t compute_IP_checksum_value(const struct IP_header* ip){
     struct IP_header ip_copy = *ip;
     
     // Set checksum field to zero for calculation
+    //cout<<"checksum network "<<ip_copy.ip_checksum<<endl;
     ip_copy.ip_checksum = 0;
     uint16_t* ptr = (uint16_t*) &ip_copy;
 
     // Sum all 16-bit words in the header
-    for (int i = 0; i < sizeof(struct IP_header) / 2; i++) {
+    uint16_t  ip_header_len_bytes = ip_copy.header_length * 4;
+    //cout<<"header len in 16 "<< ip_header_len_bytes/2<<endl;
+    //cout<<"check "<<sizeof(struct IP_header) / 2<<endl;
+    for (int i = 0; i < ip_header_len_bytes / 2; i++) {
+        
         sum += *ptr++;
+        cout<<sum<<endl;
     }
 
     // Add carry bits to sum
@@ -86,9 +92,9 @@ uint16_t compute_IP_checksum_value(const struct IP_header* ip){
     }
 
     // Return one's complement of sum
-    //cout<<"the sum is"<<sum<<endl;
-    //cout<<"total sum should be"<< ~(sum + ~sum)<<endl;
-    return ~sum;
+    cout<<"the sum is"<<sum<<endl;
+    cout<<"complement of sum is"<< ntohs(~(sum))<<endl;
+    return ntohs(~sum);
 }
 uint32_t calculate_checksum(uint16_t* ptr, size_t length) {
     uint32_t sum = 0;
@@ -106,7 +112,6 @@ uint16_t compute_TCP_checksum(const struct IP_header* ip, const struct TCP_heade
     struct TCP_header tcp_copy = *tcp;
     //uint16_t old_checksum = tcp->TCP_checksum;
     tcp_copy.TCP_checksum = 0;
-
     // Calculate the checksum of the TCP header and payload
     uint32_t sum = calculate_checksum((uint16_t*) &tcp_copy, sizeof(struct TCP_header) / 2) +
                    calculate_checksum((uint16_t*)payload, payload_length / 2);
@@ -115,8 +120,8 @@ uint16_t compute_TCP_checksum(const struct IP_header* ip, const struct TCP_heade
     // Add the checksum of the pseudo-header to the sum
     uint16_t pseudo_header[6];
 
-    uint32_t host_source_ip = ntohl(ip->source_ip);
-    uint32_t host_dest_ip = ntohl(ip->dest_ip);
+    uint32_t host_source_ip = ip->source_ip;
+    uint32_t host_dest_ip = ip->dest_ip;
     pseudo_header[0] = host_source_ip >> 16;
     pseudo_header[1] = host_source_ip & 0xFFFF;
     pseudo_header[2] = host_dest_ip >> 16;
@@ -133,13 +138,15 @@ uint16_t compute_TCP_checksum(const struct IP_header* ip, const struct TCP_heade
     }
     
     // Return one's complement of sum
-    return ~sum;
+    return ntohs(~sum);
 }
 
 uint16_t compute_UDP_checksum(const struct IP_header* ip, const struct UDP_header* udp, char* payload, size_t payload_length) {
     // Zero out the checksum field for calculation
     struct UDP_header udp_copy = *udp;
-    //uint16_t old_checksum = tcp->TCP_checksum;
+    uint16_t old_checksum = udp_copy.UDP_checksum;
+    cout<<"network udp "<<old_checksum<<endl;
+    cout<<"host udp "<<ntohs(old_checksum)<<endl;
     udp_copy.UDP_checksum = 0;
 
     // Calculate the checksum of the TCP header and payload
@@ -162,8 +169,8 @@ uint16_t compute_UDP_checksum(const struct IP_header* ip, const struct UDP_heade
     uint16_t val = htons(ip->source_ip & 0xFFFF);
     cout<< val <<endl;
     */
-    uint32_t host_source_ip = ntohl(ip->source_ip);
-    uint32_t host_dest_ip = ntohl(ip->dest_ip);
+    uint32_t host_source_ip = ip->source_ip;
+    uint32_t host_dest_ip = ip->dest_ip;
     pseudo_header[0] = host_source_ip >> 16;
     pseudo_header[1] = host_source_ip & 0xFFFF;
     pseudo_header[2] = host_dest_ip >> 16;
@@ -180,7 +187,7 @@ uint16_t compute_UDP_checksum(const struct IP_header* ip, const struct UDP_heade
     }
     //printf("final sum is %u\n", sum);
     // Return one's complement of sum
-    return ~sum;
+    return ntohs(~sum);
 }
 
 pair<uint32_t, uint16_t> parseIPAndPort(const string& input) {
@@ -222,12 +229,12 @@ int processIPPacket(char* buffer, size_t length, string& final_source_ip_and_por
     char dest_ip_str[INET_ADDRSTRLEN];
     strncpy(source_ip_str, inet_ntoa(source_ip), INET_ADDRSTRLEN);
     strncpy(dest_ip_str, inet_ntoa(dest_ip), INET_ADDRSTRLEN);
-
+    cout<<"in function"<<endl;
     //test if its right:
-    printf("IP packet: version = %u, header_length = %u, total_length = %u, protocol = %u, ip_checksum = %u, TTL= %u, source_ip=%u, dest_ip =%u\n",
+    fprintf(stdout,"IP packet: version = %u, header_length = %u, total_length = %u, protocol = %u, ip_checksum = %u, TTL= %u, source_ip=%u, dest_ip =%u\n",
            version, header_length, total_length, protocol, ip_checksum, TTL, ntohl(source_ip.s_addr), ntohl(dest_ip.s_addr));
-    printf("source_ip = %s, dest_ip = %s\n", source_ip_str, dest_ip_str);
-
+    fprintf(stdout, "source_ip = %s, dest_ip = %s\n", source_ip_str, dest_ip_str);
+    cout<<"end"<<endl;
     //check IP TTL, drop if TTL=0 or 1
     if (TTL <= 1){
         printf("packet dropped due to to TTL=0 or 1\n");
@@ -237,8 +244,8 @@ int processIPPacket(char* buffer, size_t length, string& final_source_ip_and_por
     printf("performing checksums\n");
     //compute actual checksum value
     uint16_t actual_checksum = compute_IP_checksum_value(ip);
-    //cout<<"actual checksum is " << actual_checksum<<endl;
-    //cout <<"ip check sum is "<<ip_checksum<<endl;
+    cout<<"actual checksum is " << actual_checksum<<endl;
+    cout <<"ip check sum is "<<ip_checksum<<endl;
     //perform IP header checksum
     //cout<<verify_IP<<endl;
     if (actual_checksum != ip_checksum){
@@ -282,7 +289,8 @@ int processIPPacket(char* buffer, size_t length, string& final_source_ip_and_por
 
         uint16_t transport_header_length = sizeof(struct UDP_header);
         uint16_t actual_payload_length = payload_length - transport_header_length;
-
+        cout<<"transport header length: "<<transport_header_length<<endl;
+        cout<<"actual payload_length: "<<actual_payload_length<<endl;
         //printf("transport header length is %u\n", transport_header_length);
         uint16_t transport_actual_checksum = compute_UDP_checksum(ip, udp, buffer + header_len_bytes + transport_header_length, actual_payload_length);
         cout<<"transport checksum: "<<transport_actual_checksum<<endl;
@@ -327,6 +335,7 @@ int processIPPacket(char* buffer, size_t length, string& final_source_ip_and_por
         uint16_t modified_ip_checksum = compute_IP_checksum_value(modify_ip);
         modify_ip->ip_checksum = htons(modified_ip_checksum);
         cout<<"new ip is "<<ntohl(modify_ip->source_ip)<<" new checksum is "<<ntohs(modify_ip->ip_checksum)<<endl;
+        cout<<"new ip string is " <<final_source_ip_and_port<<endl;
         return 0;
     }
     string original_source_str = source_ip_string +" "+source_port_string;
